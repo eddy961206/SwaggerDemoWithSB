1b8668c67d907466a481c7c1bd0f2f3e36f13c7d
eddy961206 <skykum2018@gmail.com>
2025-01-21 10:29:44 +0900

    스프링부트Swagger활용 프로젝트 설정

---
+++ src/main/java/org/example/swaggerdemowithsb/SwaggerDemoWithSbApplication.java
@@ -0,0 +1,13 @@
+package org.example.swaggerdemowithsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerDemoWithSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerDemoWithSbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/swaggerdemowithsb/SwaggerDemoWithSbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.swaggerdemowithsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerDemoWithSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
